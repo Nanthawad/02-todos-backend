cd43fc44fdcdb50032d09a62bf55a932d2969b3c
Nanthawad <5806021622132@fitm.kmutnb.ac.th>
2018-02-21 19:21:35 +0700

    Challenge 1 not success

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+import { request } from 'https';
+import { create } from 'domain';
+
 require('dotenv').config()
 const express = require('express')
 const app = express()
@@ -7,7 +10,7 @@ const mongoose = require('mongoose')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-mongoose.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}`, { useMongoClient: true })
+mongoose.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/Todo`, { useMongoClient: true })
 const port = process.env.PORT || 9999
 const Todo = require('./app/models/todo')
 
@@ -16,6 +19,46 @@ app.get('/todos', (request, response) => {
     response.json({todos})
   })
 })
+
+app.post('/post', (request, response) => {
+  let todo = new Todo(request.body)
+  todo.save((err, createTodo) => {
+    if (err) {
+        response.status(500).send(err)
+    }
+    response.status(200).send(createTodo)
+  })
+})
+
+  app.post('/put/:todoId', (request, response) => {
+    Todo.findById(request.params.todoId, (err, todo) => {
+      if (err) {
+        response.status(500).send(err)
+      } else {
+        todo.description = request.body.description || todo.description
+        todo.done = request.body.done || todo.done
+        todo.save((err, todo) => {
+          if (err) {
+            response.status(500).send(err)
+          }
+          response.status(200).send(todo)
+        })
+      }
+    })
+  })
+
+  app.post('/delete/:todoId', (request, response) => {
+    Todo.findByIdAndRemove(request.params.todoId, (err, todo) => {
+      if (err) {
+        response.status(500).send(err)
+      }
+      let res = {
+        message: 'Todo successfully delated',
+        id: todo._id
+      }
+      response.status(200).send(res)
+    })
+  })
 
 app.listen(port)
 console.log('Server is running on port: ' + port)
